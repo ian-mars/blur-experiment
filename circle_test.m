23b3929079b0d2f348324ad83850eaa66a15e656
Ian Marshall <Ian@Auri.local>
2015-07-17 16:08:10 -0700

    adding circle_test for real this time

---
+++ circle_test.m
@@ -0,0 +1,49 @@
+close all;
+clear all;
+sca;
+
+% Here we call some default settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
+
+% Get the screen numbers
+screens = Screen('Screens');
+
+% Draw to the external screen if avaliable
+screenNumber = max(screens);
+
+% Define black and white
+white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
+grey = white / 2;
+inc = white - grey;
+
+stereoMode = 4;
+%open screen
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, black,...
+    [], 32, 2,stereoMode, [],  kPsychNeed32BPCFloat);
+
+
+% Get the size of the on screen window
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+
+% Query the frame duration
+ifi = Screen('GetFlipInterval', window);
+
+% Get the centre coordinate of the window
+[xCenter, yCenter] = RectCenter(windowRect);
+
+% Set up alpha-blending for smooth (anti-aliased) lines
+Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
+
+im = imread('~/Desktop/blur_stimuli/concave_horizontal_0_center_0_0.png');
+
+imTex = Screen('MakeTexture', window, im);
+
+Screen('DrawTexture', window, imTex, [], [], 0);
+
+Screen('Flip', window);
+
+KbStrokeWait;
+
+sca;
+ 
